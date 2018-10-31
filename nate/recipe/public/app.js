c950543974317d1d9e9ed3a22406e55239bcab64
Nate <jonathankuhl@outlook.com>
2018-10-31 17:27:21 +0100

    Added my recipe

---
+++ nate/recipe/public/app.js
@@ -0,0 +1,5 @@
+// ScrollReveal({ reset: true });
+ScrollReveal().reveal('#cook-time', { duration:1000, delay: 400 });
+ScrollReveal().reveal('#ingredients', { duration:1000, delay: 800 });
+ScrollReveal().reveal('#preparation', { duration:1000, delay: 1200 });
+ScrollReveal().reveal('#citation', { duration:1000, delay: 1400 });
